98bd384f876ab889a6e7f3b415699e3e4130b014
RstoykoMadeit <rstoyko@hawk.iit.edu>
2024-11-05 23:20:34 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 function init(){
 //add your javascrip between these two lines of code
+ var  button = document.getElementById('entryinput');
  
+ function showMeText(){
+	 var textbox = document.getElementById('entryinput');
+	 document.getElementById('textoutput').innerHTML = textbook.value;
+	 alert("Roman Stoyko: " + textbox.value);
+ }
+ 
+ button.addEventListener('click', showMeText);
+ }
 
 
 
